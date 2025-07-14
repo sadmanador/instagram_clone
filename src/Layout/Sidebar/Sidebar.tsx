6a749716ff9236c1c374096f4378e6fb1de75b04
sadmanador <sadman.ador@gmail.com>
2025-07-15 00:07:48 +0600

    sidebar

--- src/Layout/Sidebar/Sidebar.tsx
+++ src/Layout/Sidebar/Sidebar.tsx
@@ -1,6 +1,18 @@
 import { useContext } from "react";
 import { SidebarToggleContext } from "../../context/SidebarContext/SidebarContext";
 
+import {
+  MdHomeFilled,
+  MdOutlineExplore,
+  MdSearch,
+  MdOutlineOndemandVideo,
+  MdMessage,
+} from "react-icons/md";
+
+import { FaRegHeart } from "react-icons/fa";
+
+import { FaPlusSquare } from "react-icons/fa";
+
 const Sidebar = () => {
   const { theme, setTheme, sidebar, setSidebar } =
     useContext(SidebarToggleContext);
@@ -13,22 +25,63 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="sidebar bg-amber-300">
-      <h2>Sidebar</h2>
+    <div
+      className={`sidebar ${
+        sidebar ? "" : "small-sidebar"
+      } h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 bg-yellow-300 scrollbar-track-gray-100`}
+    >
       <ul>
-        <li>
-          <a href="#home">Home</a>
+        <li className="side-link">
+          <MdHomeFilled />
+          <a href="#home" className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}>
+            Home
+          </a>
         </li>
-        <li>
-          <a onClick={() => setSidebar(!sidebar)} href="#about">
+        <li className="side-link" onClick={() => setSidebar(!sidebar)}>
+          <MdSearch />
+          <a href="#about" className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}>
             Search
           </a>
         </li>
-        <li>
-          <a href="#services">Services</a>
+        <li className="side-link">
+          <MdOutlineExplore />
+          <a href="#services" className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}>
+            Explore
+          </a>
         </li>
-        <li>
-          <a href="#contact">Contact</a>
+        <li className="side-link">
+          <MdOutlineOndemandVideo />
+          <a href="#contact" className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}>
+            Reels
+          </a>
+        </li>
+        <li className="side-link">
+          <MdMessage />
+          <a href="#contact" className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}>
+            Messages
+          </a>
+        </li>
+        <li className="side-link">
+          <FaRegHeart />
+          <a href="#contact" className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}>
+            Notification
+          </a>
+        </li>
+        <li className="side-link">
+          <FaPlusSquare />
+          <a href="#contact" className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}>
+            Create
+          </a>
+        </li>
+        <li className="side-link">
+          <img
+            src="/assets/jack.png"
+            alt=""
+            className="w-10 h-10 rounded-full"
+          />
+          <a href="#contact" className={`w-4 ${sidebar ? "" : "hidden"} ml-3`}>
+            Profile
+          </a>
         </li>
       </ul>
       <div
